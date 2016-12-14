d8f1948a5e4d210a4e39d275d1275d4b5ee2cb13
Claudio Torres <cltorresfe@gmail.com>
2016-12-14 12:23:42 -0300

    limpiando codigo

--- s_receta_productos_vp.php
+++ s_receta_productos_vp.php
@@ -1,32 +1,21 @@
-<!doctype html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <title>Autocomplete</title>
-  </head>
-  <body>
-   
-    <div><br>
-      <form id= "new_product_form">
-        <div class="ui-widget">
-          <label for="product">Productos: </label>
-          <input id="product" class="product" name="product" type="text" placeholder="buscar producto" style="width: 40%">
-          <input type="hidden" id="product_id" />
-        
-        <input id="composicion" name="comp" id="comp" type="text" placeholder="composición" style="width: 70px" >
-        <select name="hora" id="hour">
-          <option value="2">2 hrs.</option>
-          <option value="4">4 hrs.</option>
-          <option value="6">6 hrs.</option>
-          <option value="8">8 hrs.</option>
-          <option value="12">12 hrs.</option>
-          <option value="24">24 hrs.</option>
-        </select>
-        <input id="days" name="days" type="text" placeholder="días" style="width: 50px" value="">
-        <div class="btn btn-info btn-sm" id="add_product">Agregar</div>
-        </div>
-      </form>
+<div><br>
+  <form id= "new_product_form">
+    <div class="ui-widget">
+      <label for="product">Productos: </label>
+      <input id="product" class="product" name="product" type="text" placeholder="buscar producto" style="width: 40%">
+      <input type="hidden" id="product_id" />
+    
+    <input id="composicion" name="comp" id="comp" type="text" placeholder="composición" style="width: 70px" >
+    <select name="hora" id="hour">
+      <option value="2">2 hrs.</option>
+      <option value="4">4 hrs.</option>
+      <option value="6">6 hrs.</option>
+      <option value="8">8 hrs.</option>
+      <option value="12">12 hrs.</option>
+      <option value="24">24 hrs.</option>
+    </select>
+    <input id="days" name="days" type="text" placeholder="días" style="width: 50px" value="">
+    <div class="btn btn-info btn-sm" id="add_product">Agregar</div>
     </div>
-
-  </body>
-</html>
+  </form>
+</div>
