6891459b22cf47d04fd4a620090abf53487f35a0
Claudio Torres <cltorresfe@gmail.com>
2016-12-20 10:07:49 -0300

    conenctando bd para ingresar alcoholemia

--- conexion/conexion.php
+++ conexion/conexion.php
@@ -1,18 +1,18 @@
 <?php
 
-$con = mysql_connect("10.6.85.218","root","208266", "dau");
+$con = mysqli_connect("10.6.85.218","root","208266", "dau");
 if (!$con)
   {
   die('Could not connect: ' . mysql_error());
   }
-mysql_query ("SET NAMES 'utf8'");
+mysqli_query ($con, "SET NAMES 'utf8'");
 
 
-$con_ris = mysql_connect("10.6.85.218","root","208266", "dau");
+$con_ris = mysqli_connect("10.6.85.218","root","208266", "dau");
 if (!$con_ris)
   {
   die('Could not connect: ' . mysql_error());
   }
-mysql_query ("SET NAMES 'utf8'");
+mysqli_query ($con_ris, "SET NAMES 'utf8'");
 
 ?>
--- s_guarda_alcoholemia_grado_lesion_dev.php
+++ s_guarda_alcoholemia_grado_lesion_dev.php
@@ -1,11 +1,12 @@
 <?php
-
-//include_once('conexion/conexion.php');
+include_once("conexion/conexion.php");
 
 $cod_atencion = $_POST['a'];
 $codEstadoEtilico = $_POST['ee'];
 $nroFrascoAlco = $_POST['nf'];
 $codGradoLesion = $_POST['gl'];
+
+
 
 $parte = 0;
 $unidad_policia = 0;
@@ -17,60 +18,82 @@ $tec_oh =  0;
 $drogas_oh =  0;
 $rechaza_oh =  0;
 
-$parte = $_POST['parte'] == '' ? null : $_POST['parte'];
-$unidad_policia = $_POST['unidad_policia'] == '' ? null : $_POST['unidad_policia'];
-$juzgado =  $_POST['juzgado'] == '' ? null : $_POST['juzgado'];
-$condicion_transito =  $_POST['condicion_transito'] == '' ? null : $_POST['condicion_transito'];
-$placa_policia =  $_POST['placa_policia'] == '' ? null : $_POST['placa_policia'];
-$observacionOH =  $_POST['observacionOH']== '' ? null : $_POST['observacionOH'];
-$tec_oh =  $_POST['tec_oh'] == "false" ? 0 : 1;
-$drogas_oh =  $_POST['drogas_oh'] == "false" ? 0 : 1;
-$rechaza_oh =  $_POST['rechaza_oh'] == "false" ? 0 : 1;
+$parte = $_POST['parte'];
+$unidad_policia = $_POST['unidad_policia'];
+$juzgado =  $_POST['juzgado'];
+$condicion_transito =  $_POST['condicion_transito'];
+$placa_policia =  $_POST['placa_policia'];
+$observacionOH =  $_POST['observacionOH'];
+$tec_oh =  $_POST['tec_oh'];
+$drogas_oh =  $_POST['drogas_oh'];
+$rechaza_oh =  $_POST['rechaza_oh'];
 
-$fechahora = date("d-m-Y H:i");
+$fechahora = strftime( "%Y-%m-%d %H:%M:%S", time() );
 
 if($codEstadoEtilico != 0){
-	$db_database = 'dau';
-	$db_hostname = '10.6.85.218';
-	$db_username = 'root';
-	$db_password = '208266';
-
-	$mysqli = mysqli_connect($db_hostname, $db_username, $db_password, $db_database);
-	if($mysqli){
-
-		echo "conexion exitosa. <br />";
-		$sql0 = "INSERT INTO eme_dau_atencion_alcoholemia
-		(fk_atencion, cond_transito, tec, drogas, rechaza, placa_policia, parte, comiseria, juzgado, observaciones) 
-		VALUES 
-		('$cod_atencion', 
-		'$codEstadoEtilico', 
-		'$tec_oh', 
-		'$drogas_oh', 
-		'$rechaza_oh', 
-		'$placa_policia', 
-		'$parte', 
-		'$unidad_policia',
-		'$juzgado', 
-		'$observacionOH')";
-
-		$resultado=mysqli_query($mysqli,$sql0);
-		if ($resultado) {
-			echo "perfil almacenado. <br />";
-		}
-		else {
-			echo "error en la ejecución de la consulta. <br />";
-		}
 		
-		if (mysqli_close($mysqli)){ 
-			echo "desconexion realizada. <br />";
-		} 
-		else {
-			echo "error en la desconexión";
-		}
+	$sql0 = "INSERT INTO eme_dau_atencion_alcoholemia
+	(fk_atencion, cond_transito, tec, drogas, rechaza, placa_policia, parte, comiseria, juzgado, observaciones) 
+	VALUES 
+	('$cod_atencion', 
+	'$codEstadoEtilico', 
+	'$tec_oh', 
+	'$drogas_oh', 
+	'$rechaza_oh', 
+	'$placa_policia', 
+	'$parte', 
+	'$unidad_policia',
+	'$juzgado', 
+	'$observacionOH')";
 
+	file_put_contents('prueba_alcoholemia.txt', $sql0."\n", FILE_APPEND);
+	$resultado =mysqli_query($con, $sql0);
+	if ($resultado) {
+				echo "alcoholemia ingresada. <br />";
+			}
+	else {
+		echo "error en la ejecución de la consulta. <br />";
 	}
-
-	//file_put_contents('prueba_alcoholemia.txt', $sql0."\n", FILE_APPEND);
 }
 
+
+if($codGradoLesion != 0 || $codEstadoEtilico != 0){
+	$sql1 = 'update eme_dau_atencion set '; 
+	if($codGradoLesion != 0){
+		$sql1 .=	'fk_grado_lesion="'.$codGradoLesion.'" ,';
+		$sql1 .=	'informe_lesiones = "'.$codGradoLesion.'" ';
+	}
+	if($codEstadoEtilico != 0){
+		if($codGradoLesion != 0){
+			$sql1 .= ', ';
+		}
+		$sql1 .=	'fk_estado_etilico = "'.$codEstadoEtilico.'" ';
+	}
+	if(isset($nroFrascoAlco) && $nroFrascoAlco != ''){
+		if($codGradoLesion != 0 || $codEstadoEtilico != 0){
+			$sql1 .= ', ';
+		}
+		
+		$sql1 .= ' alcoholemia = "'.$nroFrascoAlco.'", fecha_alcoholemia ="'. $fechahora.'" ';
+	}
+	$sql1 .= 'where cod_aten = '.$cod_atencion;
+
+
+	$resultado = mysqli_query($con, $sql1);
+	file_put_contents('prueba_alcoholemia.txt', $sql1."\n", FILE_APPEND);
+	//echo "sql1: ".$sql1." ";
+
+	if ($resultado) {
+				echo "alcoholemia.txt ingresada. <br />";
+			}
+	else {
+		echo "error en la ejecución de la consulta. <br />";
+	}
+}
+if (mysqli_close($con)){ 
+		echo "desconexion realizada. <br />";
+	} 
+	else {
+		echo "error en la desconexión";
+	}
 ?>
